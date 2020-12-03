1b41dbc135fd63848bbf4392a13353b1472ae7ae
Felicia Segerholm <feliciasegerholm@Felicias-MacBook-Air-3.local>
2020-12-03 20:39:25 +0100

    circle moves when pressed

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+const radius = 100; //radien på cirkeln
+let x,y;
+
 
 function setup() {
-    createCanvas(400,400)
+    createCanvas(windowWidth, windowHeight); //sätter canvas ytan lika stor som fönstret
+    x = random(windowWidth); //sätter cirkeln på random ställe på x-axeln (inom fönstret)
+    y = random(windowHeight); //sätter cirkeln på random ställe på y-axeln (inom fönstret)
 }
 
 function draw(){
-    
+    background (11, 1, 40) //Bakgrundsfärgen
+    fill(235, 252, 0); //färgen på cirkeln
+    noStroke(); //tar bort bordern runt cirkeln
+    ellipse(x, y, radius*2, radius*2); //skapar cirkeln med en width & height som e dubbelt så stor som radius
+} 
+
+function mousePressed() { //funktion som sker när man klickar
+    let d = dist(mouseX, mouseY, x, y); // kollar om användaren klickat i cirkeln
+    if (d < radius) { //om användaren klickat i cirkeln 
+     x = random(windowWidth); //flytta cirkeln random på x-axeln
+     y = random(windowHeight); // flytta cirkeln random på y-axeln
+ }
 }
