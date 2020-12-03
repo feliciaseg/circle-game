550f7b5d2e7dca9809e35d7e5f2b5676b4c2722c
Felicia Segerholm <feliciasegerholm@Felicias-MacBook-Air-3.local>
2020-12-03 21:34:24 +0100

    scores

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-const radius = 100; //radien på cirkeln
-let x,y;
+let radius = 100; //radien på cirkeln
+let x, y;
+let r, g, b;
+
+let score = 0;
+
 
 
 function setup() {
     createCanvas(windowWidth, windowHeight); //sätter canvas ytan lika stor som fönstret
     x = random(windowWidth); //sätter cirkeln på random ställe på x-axeln (inom fönstret)
     y = random(windowHeight); //sätter cirkeln på random ställe på y-axeln (inom fönstret)
+    newCircle();  
 }
 
 function draw(){
     background (11, 1, 40) //Bakgrundsfärgen
-    fill(235, 252, 0); //färgen på cirkeln
+    fill(r, g, b); //färgen på cirkeln (variablerna blir random nummer)
     noStroke(); //tar bort bordern runt cirkeln
     ellipse(x, y, radius*2, radius*2); //skapar cirkeln med en width & height som e dubbelt så stor som radius
+    text("Score: " + score, 50, 50); //Skapar text med poängen, tvp sista är placering av texten
+    textSize(40)
 } 
 
 function mousePressed() { //funktion som sker när man klickar
     let d = dist(mouseX, mouseY, x, y); // kollar om användaren klickat i cirkeln
-    if (d < radius) { //om användaren klickat i cirkeln 
-     x = random(windowWidth); //flytta cirkeln random på x-axeln
-     y = random(windowHeight); // flytta cirkeln random på y-axeln
- }
+    if (d < radius) {
+        newCircle();
+        score++;
+        if (score == 10) {
+            radius /= 2;
+        } 
+        if(score == 20){
+            radius /= 3;
+        }
+        if (score == 1){
+            winner();
+        }
+    }
 }
+
+function newCircle(){
+    x = random(windowWidth); //flytta cirkeln random på x-axeln
+    y = random(windowHeight); // flytta cirkeln random på y-axeln
+    r = random(255);
+    g = random(255);
+    b = random(255);
+}
+let interval = setInterval(newCircle, 1000); //flyttar cirkeln varje sekund
+
